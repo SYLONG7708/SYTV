4df11b3bd9f9568fbb790799f2c39eca8c128636
SYLONG7708 <102249435+SYLONG7708@users.noreply.github.com>
2022-03-24 01:40:13 +0800

    Update TV.php

--- TV.php
+++ TV.php
@@ -2,6 +2,7 @@
 <fenlei>1-</fenlei>
 
 #1#
+CCTV1,http://39.135.138.59:18890/PLTV/88888910/224/3221225642/index.m3u8#
 三立都会,http://easy_001.ztpfuoodzsthwn.xyz:8888/mylive/103/333300/index.m3u8#
 #1#
 }
