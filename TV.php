89bc0b7327e45477e8eb7ac6c2cc0ca659910e9c
SYLONG7708 <102249435+SYLONG7708@users.noreply.github.com>
2022-03-24 01:53:08 +0800

    Update TV.php

--- TV.php
+++ TV.php
@@ -1,45 +1,45 @@
 {
 <fenlei>台灣</fenlei>
 #台灣頭#
-民視,rtmp://59.124.75.138/sat/tv051
-民視,http://seb.sason.top/ptv/ftv.php?id=ms
+民視,rtmp://59.124.75.138/sat/tv051#
+民視,http://seb.sason.top/ptv/ftv.php?id=ms#
 
 
-民視,rtmp://59.124.75.150/sat/tv051
-台視,rtmp://59.124.75.150/sat/tv071
-中視,rtmp://59.124.75.150/sat/tv091
-華視,rtmp://59.124.75.150/sat/tv111
+民視,rtmp://59.124.75.150/sat/tv051#
+台視,rtmp://59.124.75.150/sat/tv071#
+中視,rtmp://59.124.75.150/sat/tv091#
+華視,rtmp://59.124.75.150/sat/tv111#
 
-三立台灣台,http://125.227.210.55:15714/VideoInput/play.ts
+三立台灣台,http://125.227.210.55:15714/VideoInput/play.ts#
 
-三立都会,http://easy_001.ztpfuoodzsthwn.xyz:8888/mylive/103/333300/index.m3u8
-衛視中文台,http://easy_001.ztpfuoodzsthwn.xyz:8888/mylive/103/333660/index.m3u8
-
-
-民視台灣台,http://seb.sason.top/ptv/ftv.php?id=tw#http://212.95.133.2/tv/js.php?id=31
-民視台灣台,http://seb.sason.top/ptv/ftv.php?id=tw
-
-民視旅遊台,http://seb.sason.top/ptv/ftv.php?id=ly
-民視第一台,http://seb.sason.top/ptv/ftv.php?id=dy
-民視綜藝台,http://seb.sason.top/ptv/ftv.php?id=zy
-民視影劇台,http://seb.sason.top/ptv/ftv.php?id=yj
+三立都会,http://easy_001.ztpfuoodzsthwn.xyz:8888/mylive/103/333300/index.m3u8#
+衛視中文台,http://easy_001.ztpfuoodzsthwn.xyz:8888/mylive/103/333660/index.m3u8#
 
 
-TVBS歡樂,rtmp://59.124.75.150/sat/tv421
+民視台灣台,http://seb.sason.top/ptv/ftv.php?id=tw#http://212.95.133.2/tv/js.php?id=31#
+民視台灣台,http://seb.sason.top/ptv/ftv.php?id=tw#
 
-原住民頻道,https://streamipcf.akamaized.net/live/_definst_/smil:live.smil/chunklist_b3000.m3u8?cuac=guest&cref=https%3A%2F%2Fwww.ipcf.org.tw%2F
-
-大爱一台,https://pulltv1.wanfudaluye.com/live/tv1.m3u8
-大爱二台,https://pulltv2.wanfudaluye.com/live/tv2.m3u8
-
-緯來育樂,rtmp://59.124.75.157/sat/tv701
-緯來日本,rtmp://59.124.75.157/sat/tv771
+民視旅遊台,http://seb.sason.top/ptv/ftv.php?id=ly#
+民視第一台,http://seb.sason.top/ptv/ftv.php?id=dy#
+民視綜藝台,http://seb.sason.top/ptv/ftv.php?id=zy#
+民視影劇台,http://seb.sason.top/ptv/ftv.php?id=yj#
 
 
-東森戲劇台,http://easy_001.ztpfuoodzsthwn.xyz:8888/mylive/103/333540/index.m3u8
+TVBS歡樂,rtmp://59.124.75.150/sat/tv421#
+
+原住民頻道,https://streamipcf.akamaized.net/live/_definst_/smil:live.smil/chunklist_b3000.m3u8?cuac=guest&cref=https%3A%2F%2Fwww.ipcf.org.tw%2F#
+
+大爱一台,https://pulltv1.wanfudaluye.com/live/tv1.m3u8#
+大爱二台,https://pulltv2.wanfudaluye.com/live/tv2.m3u8#
+
+緯來育樂,rtmp://59.124.75.157/sat/tv701#
+緯來日本,rtmp://59.124.75.157/sat/tv771#
 
 
-中天娛樂,http://60.250.91.85:8585/.m3u8
-TVBS歡樂,rtmp://59.124.75.157/sat/tv421
+東森戲劇台,http://easy_001.ztpfuoodzsthwn.xyz:8888/mylive/103/333540/index.m3u8#
+
+
+中天娛樂,http://60.250.91.85:8585/.m3u8#
+TVBS歡樂,rtmp://59.124.75.157/sat/tv421#
 #台灣尾#
 }
